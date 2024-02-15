34c19879b3e0d938ce96d2bf5f6cc9f456decc64
A Akil Ganesh <akilganesh3@gmail.com>
2024-02-15 22:42:31 +0530

    New change in master

--- src/Sample.java
+++ src/Sample.java
@@ -2,9 +2,9 @@
 public class Sample {
 
 	public static void main(String args[]) {
-<<<<<<< HEAD
 		System.out.println("This is develop");
 		int a = 10;
         int f = 20;
+        int h = 50;
 	}
 }
