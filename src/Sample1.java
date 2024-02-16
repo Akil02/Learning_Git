832da7f9f487d51b5cfbbf6dd3d978869f088a64
A Akil Ganesh <akilganesh3@gmail.com>
2024-02-16 23:51:00 +0530

    new change 4

--- src/Sample1.java
+++ src/Sample1.java
@@ -4,9 +4,8 @@ public class Sample1 {
 	public static void main(String args[]) {
 		System.out.println("ans");
 		int a = 10; int b = 30;
-		System.out.println(a+b);
-		System.out.println(a-b);
 		int f = 45; //count
 		//This is just for testing
+		System.out.println("Its Okay");
 	}
 }
