b8eccf84bed778ba32e5e948d0baa3817a1eebb8
A Akil Ganesh <akilganesh3@gmail.com>
2024-02-16 23:48:58 +0530

    New change 2

--- src/Sample1.java
+++ src/Sample1.java
@@ -3,9 +3,9 @@ public class Sample1 {
 
 	public static void main(String args[]) {
 		System.out.println("ans");
-		System.out.println("Added old");
 		int a = 10; int b = 30;
 		System.out.println(a+b);
 		System.out.println(a-b);
+		int f = 45; //count
 	}
 }
