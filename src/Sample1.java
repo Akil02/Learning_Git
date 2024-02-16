cc7e7b3fa2a8cb70523c537d74643b0296e26e16
A Akil Ganesh <akilganesh3@gmail.com>
2024-02-16 23:41:16 +0530

    New change

--- src/Sample1.java
+++ src/Sample1.java
@@ -4,5 +4,7 @@ public class Sample1 {
 	public static void main(String args[]) {
 		System.out.println("ans");
 		System.out.println("Added new");
+		int a = 10; int b = 30;
+		System.out.println(a+b);
 	}
 }
