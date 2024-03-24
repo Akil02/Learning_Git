be70e218586082558a43089e040dbf54d930b8a8
A Akil Ganesh <akilganesh3@gmail.com>
2024-03-24 08:46:22 +0530

    Changes in X

--- src/Sample1.java
+++ src/Sample1.java
@@ -6,7 +6,8 @@ public class Sample1 {
 		int a = 10; int b = 30;
 		int f = 45; //count
 		//This is just for testing
-		System.out.println("Its not Okay");
+		System.out.println("Its nothing Okay");
 		System.out.println("New");
+		//New
 	}
 }
