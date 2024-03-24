f8bec3d82768ac8820b11bfb9c2932401e244f62
A Akil Ganesh <akilganesh3@gmail.com>
2024-03-24 09:04:29 +0530

    Changed one in X

--- src/Sample1.java
+++ src/Sample1.java
@@ -6,9 +6,8 @@ public class Sample1 {
 		int a = 10; int b = 30;
 		int f = 45; //count
 		//This is just for testing
-		System.out.println("Its not Okay");
+		System.out.println("Hi Man how are you");
 		System.out.println("New");
 		//New
-		int c = 0;
 	}
 }
