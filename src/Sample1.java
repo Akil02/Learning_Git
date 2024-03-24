0ca4ed38fca554c77eaf23aad5247e3b6dab9328
A Akil Ganesh <akilganesh3@gmail.com>
2024-03-24 09:05:52 +0530

    Change in Y

--- src/Sample1.java
+++ src/Sample1.java
@@ -6,7 +6,7 @@ public class Sample1 {
 		int a = 10; int b = 30;
 		int f = 45; //count
 		//This is just for testing
-		System.out.println("Its Okay");
+		System.out.println("Its Okay but hind of okay");
 		System.out.println("New");
 		//New
 	}
